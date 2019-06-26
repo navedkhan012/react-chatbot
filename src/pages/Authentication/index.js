c2c6eb768b744e44bd202cef634e5b0806f4bdf8
sauzy34 <sonugupta34@gmail.com>
2019-06-26 22:40:29 +0530

    fix

--- src/pages/Authentication/index.js
+++
@@ -1,1 +0,0 @@
-export { default as LoginAndSignup } from './LoginAndSignup'
--- src/statics/Colors.js
+++
@@ -1,7 +0,0 @@
-export default {
-  primary: '#dc3545',
-  secondary: '#212529',
-  white: '#fff',
-  black: '#000',
-  grey: '#ddd'
-}
